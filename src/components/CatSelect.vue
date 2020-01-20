1f97167868da0c4b9753fb8e96998c21594e17c8
Brian Rex Ortiz <brianrexortiz@yahoo.com>
2020-01-20 08:53:08 +0800

    [FEAT] Display cat details in another page
    -Generate API query based on router params
    -Get cat details on page load
    -Modified CatSelect component to select a current cat breed based on url query when going back to home page

--- src/components/CatSelect.vue
+++ src/components/CatSelect.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <p>Breed</p>
-        <select v-model="selected" v-on:change="selectCat">
-            <option selected>Select Breed</option>
+        <select v-model="selected" v-on:change="selectCat(selected)">
+            <option>Select Breed</option>
             <option v-bind:key="breed.id" v-for="breed in breeds" :value="breed.id">{{ breed.name }}</option>
         </select>
         <CatCard v-bind:cats="cats" />
@@ -22,18 +22,23 @@ export default {
         return {
             breeds: [],
             cats: [],
-            selected: ''
+            selected: 'Select Breed'
         }
     },
     methods: {
-        selectCat () {
-            const breed = this.selected;
+        selectCat (breed) {
             axios.get(`https://api.thecatapi.com/v1/images/search?page=1&limit=10&breed_id=${breed}`)
                 .then(res => this.cats = res.data)
                 .catch();
         }
     },
     created () {
+        const breed = this.$route.query.breed
+        if (breed) {
+            this.selected = breed
+            this.selectCat(breed)
+        }
+
         axios.get('https://api.thecatapi.com/v1/breeds')
             .then(res => this.breeds = res.data)
             .catch();
--- src/views/CatPage.vue
+++ src/views/CatPage.vue
@@ -2,17 +2,42 @@
   <div class="about">
     <div>
       <button @click="goBack">Back</button>
+      <img v-bind:src="cat.url" alt="Cat Image">
+      <h3>{{ cat.breeds[0].name }}</h3>
+      <h5>Origin: {{ cat.breeds[0].origin }}</h5>
+      <h6>{{ cat.breeds[0].temperament }}</h6>
+      <p>{{ cat.breeds[0].description }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'CatPage',
+  data () {
+    return {
+      cat: {}
+    }
+  },
   methods: {
     goBack () {
-      this.$router.push({ name: 'home', query: { breed: 'private' } })
+      const breed = this.cat.breeds[0].id
+      this.$router.push({ name: 'home', query: { breed: breed } })
     }
-  }
+  },
+  created () {
+        const imageId = this.$route.params.imageId;
+        axios.get(`https://api.thecatapi.com/v1/images/${imageId}`)
+            .then(res => this.cat = res.data)
+            .catch();
+    }
 }
 </script>
+
+<style scoped>
+    img {
+        height: 50vh;
+    }
+</style>
